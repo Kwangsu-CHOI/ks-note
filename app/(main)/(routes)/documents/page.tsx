4b16df5a2d92caa4f1cd0b417571a8868301c7ce
Kwangsu-CHOI <wisecks@gmail.com>
2024-08-29 15:03:59 +1000

    first commit

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const DocumentsPage = () => {
+	const { user } = useUser();
+	const router = useRouter();
+
+	const create = useMutation(api.documents.create);
+
+	const onCreate = () => {
+		const promise = create({ title: "Untitled" }).then((documentId) =>
+			router.push(`/documents/${documentId}`)
+		);
+
+		toast.promise(promise, {
+			loading: "Creating a new note",
+			success: "New note created👍",
+			error: "Failed to create a new note😥",
+		});
+	};
+
+	return (
+		<div className="h-full flex flex-col items-center justify-center space-y-4">
+			<Image
+				src="/empty-light.png"
+				height="160"
+				width="160"
+				alt="empty"
+				className="dark:hidden"
+			/>
+			<Image
+				src="/empty-dark.png"
+				height="160"
+				width="160"
+				alt="empty"
+				className="hidden dark:block"
+			/>
+			<h2 className="text-lg font-medium">
+				Welcome to {user?.firstName}&apos;s note!
+			</h2>
+			<Button onClick={onCreate}>
+				<PlusCircle className="h-4 w-4 mr-2" />
+				Create a note
+			</Button>
+		</div>
+	);
+};
+export default DocumentsPage;
